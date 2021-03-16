b18e2beb003ba4153eab16562fa617b15151efed
Linxi Xu <xulinxi@sas.upenn.edu>
2021-03-16 12:59:30 -0400

    Finished Signing in and Signing Up

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -22,9 +23,31 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error!.localizedDescription)")
+            }
+        }
     }
     
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error!.localizedDescription)")
+            }
+        }
+        
     }
     
     /*
