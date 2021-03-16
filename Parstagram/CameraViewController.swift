eb6358a2124e27f62b7a8f26c9be026d4d88398b
Linxi Xu <xulinxi@sas.upenn.edu>
2021-03-16 13:59:32 -0400

    finished saving the posts

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -20,6 +21,27 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else {
+                print("Error!")
+            }
+        }
+        
+        
     }
     
 
