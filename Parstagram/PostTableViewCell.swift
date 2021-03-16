1d5b2777ce54193487c55c0383e9557f394e3d49
Linxi Xu <xulinxi@sas.upenn.edu>
2021-03-16 16:27:20 -0400

    Finished initialing the post cells

---
+++ Parstagram/PostTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram
+//
+//  Created by Linxi Xu on 3/16/21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
